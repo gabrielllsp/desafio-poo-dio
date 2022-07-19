39237101023802e56517ead027dbb73a70696e17
Gabrielllsp <gabriel.amoreira@outlook.com>
2022-07-19 16:29:28 -0300

    Adicionando Novos nomes e cursos.

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,16 @@ public class Main {
         curso2.setDescricao("descrição curso java script");
         curso2.setCargaHoraria(4);
 
+        Curso curso3 = new Curso();
+        curso1.setTitulo("curso Sql");
+        curso1.setDescricao("descrição curso Sql");
+        curso1.setCargaHoraria(8);
+
+        Curso curso4 = new Curso();
+        curso1.setTitulo("curso Php");
+        curso1.setDescricao("descrição curso Php");
+        curso1.setCargaHoraria(8);
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
@@ -40,6 +50,8 @@ public class Main {
         System.out.println("Conteúdos Inscritos Gabriel " + devGabriel.getConteudosInscritos());
         devGabriel.progredir();
         devGabriel.progredir();
+        devGabriel.progredir();
+        devGabriel.progredir();
         System.out.println("-");
         System.out.println("Conteúdos Inscritos " + devGabriel.getConteudosInscritos());
         System.out.println("Conteúdos Concluidos " + devGabriel.getConteudosConcluidos());
@@ -52,11 +64,34 @@ public class Main {
         devJoao.inscreverBootcamp(bootcamp);
         System.out.println("Conteúdos Inscritos João " + devJoao.getConteudosInscritos());
         devJoao.progredir();
-        devJoao.progredir();
-        devJoao.progredir();
         System.out.println("-");
         System.out.println("Conteúdos Inscritos " + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluidos " + devJoao.getConteudosConcluidos());
         System.out.print("XP: " + devJoao.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devRoberval = new Dev();
+        devRoberval.setNome("Roberval");
+        devRoberval.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Roberval " + devRoberval.getConteudosInscritos());
+        devRoberval.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos " + devRoberval.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + devRoberval.getConteudosConcluidos());
+        System.out.print("XP: " + devRoberval.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devJuliana = new Dev();
+        devJuliana.setNome("Juliana");
+        devJuliana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Juliana " + devJuliana.getConteudosInscritos());
+        devJuliana.progredir();
+        devJuliana.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos " + devJuliana.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + devJuliana.getConteudosConcluidos());
+        System.out.print("XP: " + devJuliana.calcularTotalXp());
     }
 }
